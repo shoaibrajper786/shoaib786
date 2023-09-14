f1ad7bd7af6fdbc3853c853b9731625d3026574d
shoaibrajper786 <144035150+shoaibrajper786@users.noreply.github.com>
2023-09-14 07:34:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var arr=[]
+
+var std=["shoaib","azhar","ahsan"]
+console.log(std);
+
+var number=["1","2","3","4"]
+console.log(number);
+
+var bolean=["true", "false" ]
+console.log(bolean);
+
+var mixed=["a","b","1","2","true","false"]
+console.log(mixed);
+
+var qualification=["ssc","hsc","bcs","cs","bcom"]
+console.log(qualification);
+
+var stdname=["kamran","ahsan","zohaib"]
+ var stdmarks=["430","470","350"]
+var totalmarks=["500"]
+ var std1per= stdmarks[0] / totalmarks*100
+ console.log(std1per);
+ var std2per=stdmarks[1]/totalmarks*100
+ console.log(std2per);
+ var std3per= stdmarks[2]/totalmarks*100
+ console.log(std3per);
+
+ var color=["orange","yellow","blue","pink","green"]
+ console.log(color);
+
+ var user = prompt["a. Ask the user what color he/she wants to add to the"]
+  color.unshift("blue");
+
+  var user=prompt["b. Ask the user what color he/she wants to add to the end"]
+  color.push("orange");
+
+  var user=prompt["Add two more color to the beginning of the array"]
+  color.unshift("orange","green");
+
+  var user=prompt["Delete the first color in the array."]
+  color.shift("")
+
+ var cityname=["lahore","karachi","nawabshah","hyderabad"]
+ console.log("cityname");
+
+ var arr=["this","is","my","cat"]
+ console.log("arr")
+ arr.join("");
+
+
